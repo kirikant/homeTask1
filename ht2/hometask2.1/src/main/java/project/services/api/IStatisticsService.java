014cec5a87c5a39564f8715c4dc32c1117a62e44
Antipov Kirill <kirikant@yandex.ru>
2022-03-30 09:39:23 +0300

    remove old version

--- ht2/hometask2.1/src/main/java/project/services/api/IStatisticsService.java
+++
@@ -1,8 +0,0 @@
-package project.services.api;
-
-public interface IStatisticsService {
-    void addSession();
-    void removeSession();
-    long getMessagesAmount();
-    long getUsersAmount();
-}
